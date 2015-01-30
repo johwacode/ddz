a0c53bfc0ca63d12c6a5aa0488220f14e17cc998
Johannes <johannes_wandsleb@freenet.de>
2015-01-30 02:42:41 +0100

    Slider und 50% der Texte eingefügt
    ToDo: pageinator aktivieren, restliche Texte+Bilder, außerdem: siehe
    voriger commit

--- routing.js
+++ routing.js
@@ -1,3 +1,79 @@
+/***** ######## data ######## *****/
+var buchauswahl = { 
+		  headline: "Die Auswahl des Buches",
+		  image: "buchwahl",
+		  content: [
+            { title: "Unsere Aufgabe",
+		      image: "buchwahl",
+		      text: "Das Dresdner Digitalisierungszentrum der SLUB (DDZ) ist eines der führenden deutschen Zentren der Massendigitalisierung in öffentlicher Hand. Mit verschiedenen Partnern arbeitet das DDZ an zahlreichen Drittmittelprojekten zur retrospektiven Digitalisierung. Es ist maßgeblich an der Entwicklung von Robotertechnologien und Open Source-Softwarelösungen beteiligt. Darüber hinaus berät und unterstützt es Einrichtungen im Freistaat Sachsen und aus anderen Regionen bei der Realisierung von Digitalisierungs- und Erschließungsprojekten. Ebenso obliegt dem DDZ die Koordinierung der sächsischen Beiträge zur Europäischen und Deutschen Digitalen Bibliothek." },
+            { title: "Welche Bücher kommen infrage?",
+		      image: "buchwahl",
+		      text: "Heutzutage werden immer mehr Bücher digitalisiert. Als Ziel hat es das Buch länger haltbar zu machen, aber auch für alle Menschen verfügbar zu machen. Meistens werden hier die Bücher zur Digitalisierung im Rahmen eines Projektes ausgewählt. Dabei muss man den Fakt beachten, dass das Urheberrecht erst 70 Jahren nach dem Tod erlischt und man die Werke digitalisieren kann. " },
+            { title: "Ein paar Daten",
+		      image: "buchwahl",
+		      text: "Im Jahr werden in der SLUB ca. 15.000 – 20.000 Bücher digitalisiert, was ungefähr 2.000.000 bis 3.000.000 Seiten entspricht. Jedes Buch wird erst geprüft und festgestellt, ob es überhaupt digitalisiert werden darf, im anderen Fall muss es erst restauriert werden." },
+			{ title: "Prognose",
+			  image: "buchwahl",
+			  text: "Wir schätzen, dass der Mengen-Digitalisierungsprozess in weniger als 5 Jahren ein Ende haben wird. Bis dahin werden unsere ca. 7 festangestellten-, Zeit- und Projektmitarbeiter weiter mit unseren 18 Digitalisierungsgeräten Fotos, Bücher, Karten und weiteres abfotografieren. Übrigens die Kosten pro Standard digitalisierte Seite (Roboter + Verschleißkosten + Personalkosten) ergeben 0.14 €, bei den größeren Geräten, wo auch Karten digitalisiert werden, steigen die Kosten auf 4,20 € pro Seite."}
+          ]
+}
+
+var digitalisierung = { 
+		  headline: "Der Digitalisierungsprozess",
+		  image: "geraet",
+		  content: [
+            { title: "Unsere Technik",
+		      image: "geraet",
+		      text: "Um die unterschiedlichsten Medien in großer Stückzahl bearbeiten zu können, stehen dem Dresdner Digitalisierungszentrum vielfältige Geräte zur Verfügung." },
+            { title: "Scan-Roboter",
+		      image: "geraet",
+		      text: "Heutzutage werden immer mehr Bücher digitalisiert. Als Ziel hat es das Buch länger haltbar zu machen, aber auch für alle Menschen verfügbar zu machen. Meistens werden hier die Bücher zur Digitalisierung im Rahmen eines Projektes ausgewählt. Dabei muss man den Fakt beachten, dass das Urheberrecht erst 70 Jahren nach dem Tod erlischt und man die Werke digitalisieren kann. " },
+            { title: "HIT Vario digital XL",
+		      image: "geraet",
+		      text: "Ein paar Daten: Im Jahr werden in der SLUB ca. 15.000 – 20.000 Bücher digitalisiert, was ungefähr 2.000.000 bis 3.000.000 Seiten entspricht. Jedes Buch wird erst geprüft und festgestellt, ob es überhaupt digitalisiert werden darf, im anderen Fall muss es erst restauriert werden." },
+			{ title: "Wolfenbütteler Buchspiegel",
+		      image: "geraet",
+		      text: "Ein paar Daten: Im Jahr werden in der SLUB ca. 15.000 – 20.000 Bücher digitalisiert, was ungefähr 2.000.000 bis 3.000.000 Seiten entspricht. Jedes Buch wird erst geprüft und festgestellt, ob es überhaupt digitalisiert werden darf, im anderen Fall muss es erst restauriert werden." }
+          ]
+}
+
+var digitalesBuch = { 
+		  headline: "Das digitale Buch",
+		  image: "ebook",
+		  content: [
+            { title: "Auf zur Nachbearbeitung",
+		      image: "ebook",
+		      text: "Nachdem ca. 450 Seiten pro Stunde digitalisiert wurden, müssen diese noch am Rechner bearbeitet werden. Die Seiten werden richtig geschnitten, manchmal passiert jedoch, dass die Seite nicht richtig kopiert wurde und der ganze Vorgang muss für eine Seite wiederholt werden."
+					+ "Als Ergebnis bekommt man ein Buch in digitalen Form, das auf einem E-Book-Reader oder Geräten mit speziellen Software gelesen werden kann."},
+            { title: "Digitalisierungsprozess",
+		      image: "ebook",
+		      text: "Heutzutage werden immer mehr Bücher digitalisiert. Als Ziel hat es das Buch länger haltbar zu machen, aber auch für alle Menschen verfügbar zu machen. Meistens werden hier die Bücher zur Digitalisierung im Rahmen eines Projektes ausgewählt. Dabei muss man den Fakt beachten, dass das Urheberrecht erst 70 Jahren nach dem Tod erlischt und man die Werke digitalisieren kann. " },
+            { title: "Digitales Buch",
+		      image: "ebook",
+		      text: "Ein paar Daten: Im Jahr werden in der SLUB ca. 15.000 – 20.000 Bücher digitalisiert, was ungefähr 2.000.000 bis 3.000.000 Seiten entspricht. Jedes Buch wird erst geprüft und festgestellt, ob es überhaupt digitalisiert werden darf, im anderen Fall muss es erst restauriert werden." }
+          ]
+}
+
+var archivierung = { 
+		  headline: "Die Archivierung",
+		  image: "archiv",
+		  content: [
+            { title: "Unsere Aufgabe",
+		      image: "archiv",
+		      text: "Das Dresdner Digitalisierungszentrum der SLUB (DDZ) ist eines der führenden deutschen Zentren der Massendigitalisierung in öffentlicher Hand. Mit verschiedenen Partnern arbeitet das DDZ an zahlreichen Drittmittelprojekten zur retrospektiven Digitalisierung. Es ist maßgeblich an der Entwicklung von Robotertechnologien und Open Source-Softwarelösungen beteiligt. Darüber hinaus berät und unterstützt es Einrichtungen im Freistaat Sachsen und aus anderen Regionen bei der Realisierung von Digitalisierungs- und Erschließungsprojekten. Ebenso obliegt dem DDZ die Koordinierung der sächsischen Beiträge zur Europäischen und Deutschen Digitalen Bibliothek." },
+            { title: "Digitalisierungsprozess",
+		      image: "archiv",
+		      text: "Heutzutage werden immer mehr Bücher digitalisiert. Als Ziel hat es das Buch länger haltbar zu machen, aber auch für alle Menschen verfügbar zu machen. Meistens werden hier die Bücher zur Digitalisierung im Rahmen eines Projektes ausgewählt. Dabei muss man den Fakt beachten, dass das Urheberrecht erst 70 Jahren nach dem Tod erlischt und man die Werke digitalisieren kann. " },
+            { title: "Digitales Buch",
+		      image: "archiv",
+		      text: "Ein paar Daten: Im Jahr werden in der SLUB ca. 15.000 – 20.000 Bücher digitalisiert, was ungefähr 2.000.000 bis 3.000.000 Seiten entspricht. Jedes Buch wird erst geprüft und festgestellt, ob es überhaupt digitalisiert werden darf, im anderen Fall muss es erst restauriert werden." }
+          ]
+}
+
+
+
+/***** ######## routing ######## *****/
+
 Router.map(function(){
     this.route('home',
 	{
@@ -6,39 +82,103 @@ Router.map(function(){
 	    {categories: [
       { text: "Analoges Buch",
 		image: "buchwahl",
-		link: "buchauswahl" },
+		link: "Buchauswahl" },
       { text: "Digitalisierungsprozess",
 		image: "geraet",
-		link: "geraete" },
+		link: "Digitalisierung" },
       { text: "Digitales Buch",
 		image: "ebook",
-		link: "digitales-buch" },
+		link: "Digitales-Buch" },
       { text: "Archivierung",
 		image: "archiv",
-		link: "archivierung" }
+		link: "Archivierung" }
     ],
-		aktuelles: "bla bla blub"}
+		aktuelles: "Das derzeit bearbeitete Digitalisierungsprojekt umfasst Bücher und Karten zum Thema Lorem Ipsum."}
 	} );
 	
     this.route('buchauswahl',
-	{
-	  after: function () {
-		$(".owl-carousel").owlCarousel({
- 
-			  navigation : true, // Show next and prev buttons
-			  slideSpeed : 300,
-			  paginationSpeed : 400,
-			  singleItem:true
-		 
-			  // "singleItem:true" is a shortcut for:
-			  // items : 1, 
-			  // itemsDesktop : false,
-			  // itemsDesktopSmall : false,
-			  // itemsTablet: false,
-			  // itemsMobile : false
-		 
-		  });
-		console.log("inited");
-	  }
+	  {
+	    data: buchauswahl,
+		action: function(){
+		    this.render("Subsite");
+		    setTrigger();
+		  }
+	});
+	
+	this.route('digitalisierung',
+	  {
+	    data: digitalisierung,
+		action: function(){
+		    this.render("Subsite");
+		    setTrigger();
+		  }
+	});
+	
+	this.route('Digitales-Buch',
+	  {
+	    data: digitalesBuch,
+		action: function(){
+		    this.render("Subsite");
+		    setTrigger();
+		  }
+	});
+	
+	this.route('Archivierung',
+	  {
+	    data: archivierung,
+		action: function(){
+		    this.render("Subsite");
+		    setTrigger();
+		  }
 	});
 });
+
+
+
+function setTrigger(){
+  Template.Subsite.rendered = function() {
+    initCarousel();
+    Template.Subsite.rendered = null;
+  };
+}
+
+function initCarousel(){
+$("#owl-example").owlCarousel(
+	{
+		items: 1,
+		singleItem : true,
+		itemsScaleUp : false,
+	 
+		//Basic Speeds
+		slideSpeed : 200,
+		paginationSpeed : 800,
+		rewindSpeed : 1000,
+	 
+		//Autoplay
+		autoPlay : false,
+		stopOnHover : false,
+	 
+		// Navigation
+		navigation : false,
+		rewindNav : true,
+		scrollPerPage : false,
+	 
+		//Pagination
+		pagination : false,
+		paginationNumbers: false,
+	 
+		// Responsive 
+		responsive: true,
+		responsiveRefreshRate : 200,
+		responsiveBaseWidth: window,
+	 
+		//Mouse Events
+		dragBeforeAnimFinish : true,
+		mouseDrag : true,
+		touchDrag : true,
+	 
+		//Transitions
+		transitionStyle : false
+	}
+  );
+}
